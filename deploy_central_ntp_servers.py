9112aea8b16ef710b187d8e365b6bb4f503cbe79
IPvZero <46572101+IPvZero@users.noreply.github.com>
2021-04-12 00:47:33 +0100

    Create deploy_central_ntp_servers.py

---
+++ deploy_central_ntp_servers.py
@@ -0,0 +1,31 @@
+"""
+Author: John McGovern (IPvZero)
+"""
+import os
+import sys
+from nornir import InitNornir
+from nornir.core.filter import F
+from nornir_utils.plugins.functions import print_result
+from nornir_napalm.plugins.tasks import napalm_configure
+from ipvzero_tasks import replace_ntp
+
+config_file = sys.argv[1]
+nr = InitNornir(config_file=config_file)
+targets = nr.filter(F(site="central"))
+
+targets.inventory.defaults.username = os.getenv("USERNAME")
+targets.inventory.defaults.password = os.getenv("PASSWORD")
+
+
+def desired_ntp(task):
+    """
+    Load in custom IPvZero task
+    Replace NTP Configuration
+    """
+
+    config = replace_ntp(task)
+    task.run(task=napalm_configure, configuration=config, replace=True)
+
+
+ntp_result = targets.run(task=desired_ntp)
+print_result(ntp_result)
